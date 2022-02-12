766208378b2c30e98c9c1a66f1d1dd7b08f76142
developerdesigner18 <developer.designer18@gmail.com>
2022-02-12 07:45:11 +0530

    cheapter-One Page

--- src/component/chep1Card.jsx
+++ src/component/chep1Card.jsx
@@ -446,125 +446,95 @@ const ChepOneCard = () => {
               </Row>
             </Card.Title>
             <Row style={{ textAlign: "center", border: "1px solid white" }}>
-              <Col>For 3.0.1</Col>
-              <Col>for 3.0.2</Col>
-            </Row>
-            {/* 
-            <Row>
-              <Col lg={6} style={{ width: "460px", height: "300px" }}>
-                <Card.Img
-                  src="./images/chep1/chepOneCardFour.png"
-                  style={{ width: "100%", paddingLeft: 0 }}
-                />
-              </Col>
-              <Col lg={6}>
-                <Row style={{ paddingBottom: 15 }}></Row>
-                <Row style={{ width: 29.69, height: 19.1 }}>
+              <Col>
+                For 3.0.1
+                <Row>
+                  <Image fluid src="./images/chep1/cardThreeOne.png" />
+                </Row>
+                <Row>
                   <Image
-                    src="./images/imgArrow.svg"
-                    style={{ width: "100%", paddingLeft: 10, paddingRight: 0 }}
-                    alt="Arrow"
+                    fluid
+                    src="./images/chep1/upArrow.svg"
+                    style={{
+                      width: 26.96,
+                      height: 17.41,
+                      padding: 0,
+                      marginTop: 15,
+                      marginLeft: 3,
+                    }}
                   />
                 </Row>
-                <Row
-                  style={{
-                    textAlign: "left",
-                    color: "white",
-                    paddingLeft: 10,
-                    fontFamily: "Helvetica Neue",
-                    fontSize: 20,
-                    letterSpacing: "1px",
-                  }}
-                >
-                  IMAGE:
-                </Row>
-                <Row
-                  style={{
-                    textAlign: "left",
-                    color: "white",
-                    paddingLeft: 10,
-                    fontSize: 15,
-                    letterSpacing: "1px",
-                    fontFamily: "Helvetica Neue",
-                  }}
-                ></Row>
-                <Row
-                  style={{
-                    // textAlign: "left",
-                    color: "#20C5F7",
-                    float: "left",
-                    fontSize: 15,
-                    letterSpacing: "1px",
-                    paddingLeft: 10,
-                    width: "100%",
-                    marginBottom: "20px",
-                    fontFamily: "Helvetica Neue",
-                  }}
-                >
-                  Collections of photos from families whose children and
-                  grandchildren had disappeared. Human rights organizations
-                  estimate that over 30,000 people were disappeared.
-                </Row>
-              </Col>
-            </Row>
-            <Card.Text>
-              <Row>
-                <Col lg={5} sm={12} md={8}>
-                  <Row
+                <Row>
+                  <p
                     style={{
-                      color: "white",
-                      float: "left",
-                      fontSize: 14,
-                      paddingLeft: 10,
+                      textAlign: "left",
+                      marginTop: 10,
                       fontFamily: "Helvetica Neue",
-                      letterSpacing: "0.5px",
-                      paddingLeft: 10,
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "14px",
+                      marginBottom: "5px",
                     }}
                   >
-                    <Row style={{ letterSpacing: 1 }}>
-                      The Junta declared a so-called “Dirty War” against this
-                      enemy from within. The full extent of the atrocities
-                      committed between 1976 and 1983 is not certain.
-                    </Row>
-                    <Row style={{ paddingTop: 15 }}>
-                      The figure generally quoted, however, is that an estimated
-                      30,000 people were “disappeared,” with many others
-                      illegally imprisoned or going into exile.
-                    </Row>
-
-                    <Row
-                      style={{ paddingLeft: 0, paddingTop: 10, marginTop: 5 }}
-                    >
-                      <Col lg={4} style={{ width: 50, textAlign: "right" }}>
-                        <a href="#" role="button">
-                          <Image
-                            src="./images/btnImage.svg"
-                            style={{
-                              paddingLeft: 0,
-                              width: "100%",
-                            }}
-                            fluid
-                          />
-                        </a>
-                      </Col>
-                      <Col
-                        lg={8}
+                    Jorge Rafael Videla
+                  </p>
+                  <p
+                    style={{
+                      textAlign: "left",
+                      fontFamily: "Helvetica Neue",
+                      color: "#20C5F7",
+                      fontSize: "14px",
+                    }}
+                  >
+                    (August 2, 1925–May 17, 2013) was a General Commander of the{" "}
+                    <br />
+                    Army, member of the military junta, and dictator of
+                    Argentina from <br /> 1976 to 1981. <br /> On December 22,
+                    2010, Videla was sentenced to life in a civilian prison for{" "}
+                    <br />
+                    the deaths of 31 prisoners following his coup.
+                  </p>
+                </Row>
+                <Row style={{ paddingLeft: 0, paddingTop: 10, marginTop: 5 }}>
+                  <Col lg={4} style={{ width: 50, textAlign: "right" }}>
+                    <a href="#" role="button">
+                      <Image
+                        src="./images/btnImage.svg"
                         style={{
-                          fontFamily: "Helvetica Neue",
-                          fontSize: 12,
-                          float: "left",
-                          paddingLeft: 10,
-                          paddingTop: 5,
+                          paddingLeft: 0,
+                          width: "100%",
                         }}
-                      >
-                        LEARN About The Dirty War
-                      </Col>
-                    </Row>
-                  </Row>
-                </Col>
-                <Col lg={7} sm={12} md={4}></Col>
-              </Row>
-            </Card.Text> */}
+                        fluid
+                      />
+                    </a>
+                  </Col>
+                  <Col
+                    lg={8}
+                    style={{
+                      fontFamily: "Helvetica Neue",
+                      fontSize: 12,
+                      float: "left",
+                      color: "white",
+                      textAlign: "left",
+                      paddingTop: 5,
+                      paddingLeft: 0,
+                    }}
+                  >
+                    Learn More About Jorge Rafael Videla
+                  </Col>
+                </Row>
+              </Col>
+              <Col>
+                for 3.0.2
+                <Row>
+                  <Image fluid src="./images/chep1/cardThreeTwo.png" />
+                </Row>
+                <Row></Row>
+                <Row></Row>
+                <Row></Row>
+                <Row></Row>
+              </Col>
+            </Row>
           </Card.Body>
         </Card>
 
